f77caae35e6552cdd82bebcc1e3efb15cbe200d8
paras <parasguptapg890@gmail.com>
2025-06-20 19:32:01 +0530

    updated

--- api/server.js
+++ api/server.js
@@ -10,17 +10,9 @@ require('dotenv').config();
 const app = express();
 
 // CORS configuration
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:8080",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:8080"
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+
+
+app.use(cors({origin: true, credentials:true}))
 
 app.use(express.json());
 
